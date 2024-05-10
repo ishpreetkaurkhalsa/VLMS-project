b44c1f78717182e9f72592a57530d2510f877b4f
Ishpreet Kaur Khalsa <113664759+ishpreetkaurkhalsa@users.noreply.github.com>
2024-05-10 18:27:56 +0530

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,23 @@ import Signup from './pages/Signup'
 import Profile from './pages/Profile'
 import Navbar from './components/Navbar'
 import { AuthContextProvider } from './context/AuthContext'
+import { BrowserRouter } from 'react-router-dom'
+
 import ProtectedRoute from './components/ProtectedRoute'
 const App = () => {
   return (
     <>
-      <AuthContextProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-        </Routes>
-      </AuthContextProvider>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          </Routes>
+        </AuthContextProvider>
+      </BrowserRouter>
     </>
   )
 }
